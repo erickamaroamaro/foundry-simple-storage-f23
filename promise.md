1ec2026f4a085f7a868a7223690c970022f9e6aa
Erick <erickamaroamaro@gmail.com>
2025-02-25 21:21:19 -0300

    primise and contract

---
+++ promise.md
@@ -0,0 +1,2 @@
+I promise to never use my private key associated with my real money in plain text.
+history -c
--- src/Counter.sol
+++ src/Counter.sol
@@ -1,13 +1,20 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+/// @title Um contrato simples de contador
+/// @author Seu Nome
+/// @notice Este contrato permite incrementar e definir um número
 contract Counter {
+    /// @dev Armazena o valor atual do contador
     uint256 public number;
 
+    /// @notice Define um novo valor para o contador
+    /// @param newNumber O novo valor a ser definido
     function setNumber(uint256 newNumber) public {
         number = newNumber;
     }
 
+    /// @notice Incrementa o contador em 1
     function increment() public {
         number++;
     }
